6923f3164a14028b576511800937e692377aff67
krishpranav <krisna.pranav@gmail.com>
2021-04-22 09:30:18 +0530

    added bytes for random pic

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ var speed = prompt("Make request ever[blank] milliseconds");
 var msg = prompt("Message to HTTP server");
 
 // function for attack
+function attack() {
+    var pic = new Image();
+    var rand1 = Math.floor(Math.random() * 99999999999999999999999999999999999999999999);
+    var rand2 = Math.floor(Math.random() * 99999999999999999999999999999999999999999999);
+    
+}
