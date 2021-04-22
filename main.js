5c4c89363da25243ec30cae2e1d3eb3eaf193e9c
krishpranav <krisna.pranav@gmail.com>
2021-04-22 09:29:16 +0530

    update main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+// variables
+var target = prompt("Image URL, add / to the end");
+var speed = prompt("Make request ever[blank] milliseconds");
+var msg = prompt("Message to HTTP server");
+
+// function for attack
